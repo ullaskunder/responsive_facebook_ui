b59672d898a5ff8fcd8c3aad6e9869f04cc1c3f5
ullaskunder <ullaskunder.work@gmail.com>
2020-08-11 21:42:50 +0530

    finished homescreen

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'screens/screens.dart';
+
+import 'config/palette.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Facebook UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Palette.scaffold,
+      ),
+      home: NavScreen(),
+    );
+  }
+}
